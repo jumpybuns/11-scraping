04a55cdd880769588169fde4bb946f312cf77b2d
Jumpybuns <ethanpiercepresents@gmail.com>
2020-12-16 18:20:29 -0800

    i got the number 20

--- sql/setup.sql
+++ sql/setup.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS products;
 CREATE TABLE products (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     title TEXT NOT NULL,
-    price INTEGER,
+    price TEXT NOT NULL,
     rating TEXT NOT NULL,
     image TEXT NOT NULL,
     in_stock BOOLEAN
